66cea1065eb038eaac24ecedd5c4fc8d1ebf519f
Kadika <dryan10101@gmail.com>
2022-07-01 18:16:13 -0700

    set up get all route for users

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,2 +1,16 @@
 const router = require('express').Router();
 const { User, Post } = require('../../models');
+
+//user get slash route returns all users
+router.get('/', async (req, res) => {
+    try {
+        const allUserData = await User.findAll({
+            include: [{ model: Post }],
+        });
+        res.status(200).json(allUserData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
