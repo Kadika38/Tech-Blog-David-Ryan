f7ee4103a61e570796ee458623af0316e4875e3f
Kadika <dryan10101@gmail.com>
2022-07-01 18:53:10 -0700

    set up user post route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { User, Post } = require('../../models');
 
-//user get slash route returns all users
+//user get slash route returns all users (just used for testing, actually not used for website)
 router.get('/', async (req, res) => {
     try {
         const allUserData = await User.findAll({
@@ -12,5 +12,15 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+router.post('/', async (req, res) => {
+    try {
+        User.create(req.body).then((product) => {
+            res.status(200).json(product);
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
