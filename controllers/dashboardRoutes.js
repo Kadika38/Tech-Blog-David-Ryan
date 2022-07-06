030081f3a2b0031d0ecb698db975e62723355c51
Kadika <dryan10101@gmail.com>
2022-07-06 14:22:29 -0700

    dashboard now shows the users posts

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,9 +1,26 @@
 const router = require('express').Router();
 const withAuth = require('../utils/auth');
+const { Post } = require('../models');
 
 router.get('/', withAuth, async (req, res) => {
     try {
-        res.render('dashboard', {logged_in: req.session.logged_in});
+        const postData = await Post.findAll({
+            where: {
+                user_id: req.session.user_id
+            }
+        });
+
+        if (postData.length > 0) {
+            const posts = postData.map((project) => project.get({ plain: true }));
+            res.render('dashboard', {
+                posts,
+                logged_in: req.session.logged_in
+            });
+        } else {
+            res.render('dashboard', {
+                logged_in: req.session.logged_in
+            });
+        }
     } catch (err) {
         res.status(500).json(err);
     }
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,8 +1,1 @@
-const testthis = async () => {
-    const response = await fetch('/api/users/amiloggedin', {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' },
-    });
-}
-
-document.querySelector('#testbtn').addEventListener('click', testthis);
+//hi
